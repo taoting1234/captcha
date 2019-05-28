d5d458fc00b86f6e87d3361bce7dbf9d8f8e868b
taoting <335354289@qq.com>
2019-05-28 19:52:28 +0800

    修复bug

--- helper.py
+++ helper.py
@@ -13,10 +13,12 @@ def load_data(directory, characters):
     for file_name in file_list:
         label = file_name.split('_')[0].lower()
         img = Image.open(os.path.join(directory, file_name))
+        img = img.convert('L')
+        img = img.resize((150, 50))
         img_arr = np.array(img)
         img_arr = img_arr.reshape((img_arr.shape[0], img_arr.shape[1], -1))
         x_data.append(img_arr)
-        y_data.append([to_categorical(characters.find(label[i]), len(characters)) for i in range(4)])
+        y_data.append([to_categorical(characters.find(label[i]), len(characters)) for i in range(len(label))])
 
     x_data = np.array(x_data) / 255
     y_data = np.array(y_data)
--- train.py
+++ train.py
@@ -6,32 +6,35 @@ from helper import load_data
 
 
 def train():
-    epoch = 100  # 训练轮次
+    epoch = 1000  # 训练轮次
     batch_size = 100  # 一次输入的大小
     test_ratio = 0.1  # 测试集比例
-    captcha_len = 4  # 验证码长度
     captcha_characters = string.digits + string.ascii_lowercase  # 验证码字符
 
     directory = '/Users/taoting/Desktop/zf'  # 图片路径
+    model_name = 'zf_model.h5'
+
     x_data, y_data = load_data(directory, captcha_characters)
+    captcha_len = len(y_data)
 
     inputs = Input(shape=x_data.shape[1:])
 
     x = inputs
-    for i in range(4):
-        x = Convolution2D(32 * 2 ** i, (3, 3), padding="same", activation='relu')(x)
-        x = Convolution2D(32 * 2 ** i, (3, 3), padding="same", activation='relu')(x)
+    for i in range(3):
+        x = Convolution2D(32 * (2 ** i), (3, 3), activation='relu')(x)
+        x = Convolution2D(32 * (2 ** i), (3, 3), activation='relu')(x)
         x = MaxPooling2D((2, 2))(x)
 
+    x = Reshape((-1, 128))(x)
+    x = Bidirectional(GRU(128, return_sequences=True))(x)
     x = Flatten()(x)
+
     x = Dense(len(captcha_characters))(x)
     x = Dropout(0.25)(x)
-    x1 = Dense(len(captcha_characters), activation="softmax", name="x1")(x)
-    x2 = Dense(len(captcha_characters), activation="softmax", name="x2")(x)
-    x3 = Dense(len(captcha_characters), activation="softmax", name="x3")(x)
-    x4 = Dense(len(captcha_characters), activation="softmax", name="x4")(x)
+    x = [Dense(len(captcha_characters), activation="softmax", name="x{}".format(i))(x)
+         for i in range(captcha_len)]
 
-    model = Model(inputs=inputs, outputs=[x1, x2, x3, x4])
+    model = Model(inputs=inputs, outputs=x)
 
     model.summary()
 
@@ -40,7 +43,7 @@ def train():
     model.fit(x_data, y_data, batch_size=batch_size, epochs=epoch, shuffle=True,
               validation_split=test_ratio, callbacks=[EarlyStopping(patience=10)])
 
-    model.save('model.h5')
+    model.save(model_name)
 
 
 if __name__ == '__main__':
